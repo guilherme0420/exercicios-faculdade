276cb0f483a6a8ba7dabf9aa26004bde04e696be
guilherme0420 <gui.dias2222@gmail.com>
2024-10-30 18:48:12 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,11 @@
+var nome = prompt("Qual seu nome?")
+var idade = Number(prompt("Qual sua idade?"))
+var curso = prompt("Qual seu curso?")
+var profissão = prompt("Qual sua profissão")
+
+alert(`
+    Seu nome é: ${nome}
+    Sua idade é: ${idade}
+    Seu curos é: ${curso}
+    Sua profissão é: ${profissão}
+    `)
